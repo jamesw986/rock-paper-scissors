91c10b0d34023eb875129ecf4fe1d3ceb242b7a9
James Wildbure <jamesw986@gmail.com>
2022-01-12 22:43:34 +0000

    Added playRound() function to script.js

--- script.js
+++ script.js
@@ -2,10 +2,40 @@ function computerPlay() {
     // Randomly returns "rock", "paper" or "scissors" to simulate a computer player
     let x = Math.floor(Math.random() * 3) + 1;
     if (x === 1) {
-        return "Rock";
+        return "rock";
     } else if (x === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionL = playerSelection.toLowerCase();
+    if (playerSelectionL === computerSelection) {
+        return `Draw, you both selected ${playerSelectionL}`
+    }
+    if (playerSelectionL === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose, paper covers rock!";
+        } else if (computerSelection === "scissors") {
+            return "You win, rock crushes scissors!";
+        }
+    } else if (playerSelectionL === "paper") {
+        if (computerSelection === "rock") {
+            return "You win, paper covers rock!";
+        } else if (computerSelection === "scissors") {
+            return "You lose, scissors cuts paper!";
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return "You lose, rock crushes scissors!";
+        } else if (computerSelection === "paper") {
+            return "You win, scissors cuts paper";
+        }
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
